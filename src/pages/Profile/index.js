95ee291759012ea1d1f13eeaa74161e65545dab1
MurariSabavath <smurari1004@gmail.com>
2022-06-26 18:04:07 +0530

    update header

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useQuery } from "react-query";
+import { apiInstance } from "../../services/axiosInstance";
 
 const Profile = () => {
-  useEffect(() => {
-    console.log("hello world");
-  });
+  const { data } = useQuery("profile-data", () => apiInstance("users/get/me"));
+
+  console.log(data?.data);
   return <h1>hello world</h1>;
 };
 
