982dd1f663ff85c33a45e0528f7a0f5002a1202d
MurariSabavath <smurari1004@gmail.com>
2022-06-28 09:42:35 +0530

    resolve header invisible issue

--- src/routes/MainOutlet.js
+++ src/routes/MainOutlet.js
@@ -1,5 +1,5 @@
+import Header from "Components/Header";
 import { Outlet } from "react-router-dom";
-import { Header } from "Components/common/comment/styled";
 
 const MainOutlet = ({ theme, setTheme }) => {
   return (
