3d76104716123f3abac941b78d74ee2080bb25e4
MurariSabavath <smurari1004@gmail.com>
2022-06-28 14:28:17 +0530

    add lazy loading images

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,14 +6,24 @@ const Home = () => {
   return (
     <Container>
       <DetailsRow direction="row">
-        <img src={ask_questions} alt="ask questions" width="40%" />
+        <img
+          src={ask_questions}
+          alt="ask questions"
+          width="40%"
+          loading="lazy"
+        />
         <div>
           <h1>Stuck while coding</h1>
           <h2>Clear your doubts and other developer`s doubts</h2>
         </div>
       </DetailsRow>
       <DetailsRow direction="row-reverse">
-        <img src={publish_article} alt="post article" width="40%" />
+        <img
+          src={publish_article}
+          alt="post article"
+          width="40%"
+          loading="lazy"
+        />
         <div>
           <h1>Wanna post your article</h1>
           <h2>Share your thoughts and knowledge</h2>
